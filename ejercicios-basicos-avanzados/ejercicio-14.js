c5a227c81434e4ecace118ae47ad8c1b55a4f305
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-09-12 10:24:37 +0200

    realizado

--- ejercicios-basicos-avanzados/ejercicio-14.js
+++ ejercicios-basicos-avanzados/ejercicio-14.js
@@ -1,4 +1,5 @@
-/*Contador de repeticiones: Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+/*Contador de repeticiones: Crea una función que nos devuelva el número de veces que se repite cada una 
+de las palabras que lo conforma.
 Por ejemplo, que devuelva: code:4, repeat: 1, eat: 1,...
 Podéis conformar el formato que queráis para la devolución del resultado siempre y cuando sea claro.
 Puedes usar este array para probar tu función: */
@@ -18,5 +19,25 @@ const words = [
 ];
 // 'code' 4 'repeat' 1 'eat' 1 'sleep' 3 'enjoy' 2 
 function repeatCounter(list) {
-  // Completar
+  const contadorPalabra = [];
+
+  for (const palabra of list) {
+    let palabraEncontrada = false;
+
+      for (let i = 0; i < contadorPalabra.length; i++) {
+        if (contadorPalabra[i][0] === palabra) {
+          contadorPalabra[i][1]++;
+          palabraEncontrada = true;
+          break;
+        }
+      }
+
+    if (!palabraEncontrada) {
+      contadorPalabra.push([palabra, 1]);
+      }
+    }
+      console.log(contadorPalabra);
+    
 }
+
+repeatCounter(words);
