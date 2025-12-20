d83aad8e6fbc68af187aace01672932e812a6974
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-12-20 21:22:31 +0100

    Ejercicio 40 resuelto

--- ejercicios-basicos-avanzados/ejercicio-40.js
+++ ejercicios-basicos-avanzados/ejercicio-40.js
@@ -3,10 +3,6 @@
 del array cuando el valor del array sea igual al valor del texto que enviaste como parámetro.
 Haz varios ejemplos y compruébalos.*/
 
-function findArrayIndex(array, text) {
-    //Tu codigo
-}
-
 const mainCharacters = [
     "Luke",
     "Leia",
@@ -17,13 +13,40 @@ const mainCharacters = [
     "Obi-Wan",
 ];
 
+function findArrayIndex(array, text) {
+    for(elemento of array) {
+        if (elemento === text){
+            return array.indexOf(text)
+        }
+    }
+    if (elemento !== text){
+        return `Personaje no encontrado`
+    }
+}
+
+console.log(findArrayIndex(mainCharacters, 'Leia')) // Sale 1 - ok
+console.log(findArrayIndex(mainCharacters, 'Joa')) // Personaje no encontrado
+
 /*2. Usando la función anterior benefíciate de poder conocer el indice del array para crear una función llamada removeItem que,
 pasándole un array y un texto como parámetros (los mismos parámetros que en el anterior ejercicio), llame a la función 
 anteriormente creada findArrayIndex y obtén el indice para posteriormente usar la función de javascript .splice() para 
 eliminar el elemento del array.*/
-
 /*Es decir, tienes que crear una función que elimine elementos del array y retorne el nuevo array sin el elemento, apoyándote 
-en findArrayIndex.
-Finalmente retorna el array.
-De nuevo haz varios ejemplos para practicar y comprueba que funcionan correctamente.*/
+en findArrayIndex. Finalmente retorna el array.De nuevo haz varios ejemplos para practicar y comprueba que funcionan correctamente.*/
+
+const removeTextItem = (array, text) => {
+    let newmainCharacters = []
+    
+    for (element of array) {
+        newmainCharacters.push(element) 
+    }
+    i = findArrayIndex(newmainCharacters, text)
+    newmainCharacters.splice(i, 1)
+    return newmainCharacters
+}
+
+console.log(removeTextItem (mainCharacters, "Obi-Wan"))
+console.log(removeTextItem (mainCharacters, "Luke"))
+
+
 
