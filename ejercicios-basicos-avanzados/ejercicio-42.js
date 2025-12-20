83bc6a1171d0479fb56990ab7b2e4f63a0be2064
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-12-20 21:06:08 +0100

    Ejercicio 42 resuelto

--- ejercicios-basicos-avanzados/ejercicio-42.js
+++ ejercicios-basicos-avanzados/ejercicio-42.js
@@ -9,3 +9,11 @@ const fantasticFour = [
     "La mujer invisible",
     "La cosa",
 ];
+
+const swap = (array, parametro1, parametro2) => {
+    let guardadoParamInicial = array[parametro1]
+    array[parametro1] = array[parametro2]
+    array[parametro2] = guardadoParamInicial
+    return array
+}
+console.log(swap(fantasticFour, 0, 2))
