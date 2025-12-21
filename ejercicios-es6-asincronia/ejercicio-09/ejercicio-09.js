ecceabc66a67099ea530d0e82fc7e6d9ff50a023
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-12-21 19:20:43 +0100

    ES6: Ejercicio 9 - resuelto

--- ejercicios-es6-asincronia/ejercicio-09/ejercicio-09.js
+++ ejercicios-es6-asincronia/ejercicio-09/ejercicio-09.js
@@ -1,6 +1,23 @@
-const nuevosPokemon = pokemons.map((polemon) =>({
-    name: pokemon.name,
-    id: pokemon.id,
-    image: pokemon.sprite.world_dream.front
-}))
-console.log(nuevosPokemon)
+const getRandomPokemon = async () => {
+    const randomId = Math.floor(Math.random() * 151) + 1;
+    
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const pokemonData = await response.json();
+        renderPokemon(pokemonData);
+    } catch (error) {
+        console.error("Error al obtener el Pokémon:", error);
+    }
+};
+
+const renderPokemon = (pokemon) => {
+    const imgElement = document.querySelector(".random-image");
+    imgElement.src = pokemon.sprites.other['official-artwork'].front_default;
+    imgElement.alt = pokemon.name;
+    
+    const name = document.createElement("h1");
+    name.textContent = pokemon.name.toUpperCase();
+    document.body.appendChild(name);
+};
+
+getRandomPokemon();
